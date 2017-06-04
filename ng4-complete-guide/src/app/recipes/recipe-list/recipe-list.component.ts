cf743163c91600fe10345ebcde9b24dff945ff93
Matt Walker <matthewwalker101@gmail.com>
2017-06-04 21:13:58 +0200

    Up to prj-basics-11-working-on-the-shopping-list

--- ng4-complete-guide/src/app/recipes/recipe-list/recipe-list.component.ts
+++ ng4-complete-guide/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+	selector: 'app-recipe-list',
+	templateUrl: './recipe-list.component.html',
+	styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+	recipes: Recipe[] = [
+		new Recipe('A Test Recipe', 'This is simply a test', 'https://images.pexels.com/photos/46239/salmon-dish-food-meal-46239.jpeg?w=940&h=650&auto=compress&cs=tinysrgb'),
+		new Recipe('Another Recipe', 'This is also a test', 'https://images.pexels.com/photos/2215/food-salad-healthy-vegetables.jpg?w=940&h=650&auto=compress&cs=tinysrgb')
+	];
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
 }
