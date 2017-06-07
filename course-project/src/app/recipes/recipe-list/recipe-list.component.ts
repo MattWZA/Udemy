79e9e3f607ccca7484f35be4c6a2435c17997a48
Matt Walker <matthewwalker101@gmail.com>
2017-06-07 12:48:42 +0200

    Done section 8

--- course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,14 +7,20 @@ import { Recipe } from '../recipe.model';
 	styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+	@Output() recipeWasSelected = new EventEmitter<Recipe>();
+
 	recipes: Recipe[] = [
 		new Recipe('A Test Recipe', 'This is simply a test', 'https://images.pexels.com/photos/46239/salmon-dish-food-meal-46239.jpeg?w=940&h=650&auto=compress&cs=tinysrgb'),
-		new Recipe('Another Recipe', 'This is also a test', 'https://images.pexels.com/photos/2215/food-salad-healthy-vegetables.jpg?w=940&h=650&auto=compress&cs=tinysrgb')
-	];
+		new Recipe('Another Test Recipe', 'This is also a test', 'https://images.pexels.com/photos/2215/food-salad-healthy-vegetables.jpg?w=940&h=650&auto=compress&cs=tinysrgb')
+	];	
 
 	constructor() { }
 
 	ngOnInit() {
+	}
+
+	onRecipeSelected(recipe: Recipe){
+		this.recipeWasSelected.emit(recipe);
 	}
 
 }
